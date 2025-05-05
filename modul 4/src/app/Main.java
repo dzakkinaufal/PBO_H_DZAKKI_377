b561cffc5fef6da5922f123798ab1e8f95dec7b2
dzakkinaufal <ndzakkinaufal@gmail.com>
2025-05-06 01:45:10 +0700

    Add files via upload

---
+++ modul 4/src/app/Main.java
@@ -0,0 +1,32 @@
+package app;
+
+import perpustakaan.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Buku buku1 = new NonFiksi("Madilog", "Tan Malaka", "Sejarah & Ilmu Pengetahuan");
+        Buku buku2 = new Fiksi("Hainuwele: Sang Putri Kelapa", "Lilis Hu", "Dongeng");
+
+        buku1.displayInfo();
+        buku2.displayInfo();
+
+        System.out.println();
+
+
+        Anggota anggota1 = new Anggota("Dzakki", "H377");
+        Anggota anggota2 = new Anggota("Alfa", "H362");
+
+        anggota1.displayInfo();
+        anggota2.displayInfo();
+
+        System.out.println();
+
+        anggota1.pinjamBuku("Madilog");
+        anggota2.pinjamBuku("Hainuwele: Sang Putri Kelapa", 7);
+
+        System.out.println();
+
+        anggota1.kembalikanBuku("Madilog");
+        anggota2.kembalikanBuku("Hainuwele: Sang Putri Kelapa");
+    }
+}
---
+++ modul 4/src/perpustakaan/Anggota.java
@@ -0,0 +1,27 @@
+package perpustakaan;
+
+public class Anggota implements Peminjaman {
+    private String nama;
+    private String idAnggota;
+
+    public Anggota(String nama, String idAnggota) {
+        this.nama = nama;
+        this.idAnggota = idAnggota;
+    }
+
+    public void displayInfo() {
+        System.out.println("Anggota: " + nama + " (ID: " + idAnggota + ")");
+    }
+
+    public void pinjamBuku(String judul) {
+        System.out.println(nama + " meminjam buku berjudul: " + judul);
+    }
+
+    public void pinjamBuku(String judul, int durasi) {
+        System.out.println(nama + " meminjam buku " + judul + " selama " + durasi + " hari.");
+    }
+
+    public void kembalikanBuku(String judul) {
+        System.out.println(nama + " mengembalikan buku berjudul: " + judul);
+    }
+}
